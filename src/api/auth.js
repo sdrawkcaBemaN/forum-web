f80fcb6b9a13c022ad2b75b437c3cf51d75d0645
Jayaku Briliantio <jayakubriliantio@gmail.com>
2025-06-04 15:23:49 +0700

    feat: reset password

--- src/api/auth.js
+++ src/api/auth.js
@@ -25,46 +25,149 @@ export async function getAuthenticatedUser() {
   }
 }
 
-export async function logout(){
-    try{
-        const response = await fetch("http://127.0.0.1:5000/auth/user/logout",{
-            method: "POST",
-            credentials: "include",
-        });
-
-        if (response.ok) {
-            return {
-              error: null,
-              data: null,
-            };
-          } else {
-            const error = await response.json();
-            return {
-              error: error.error,
-              data: null,
-            };
-          }
-        } catch (error) {
-          return {
-            error: error.message,
-            data: null,
-          };
-    }
-}
-
-
-export async function forgetPassword() {
+export async function login(data) {
   try {
-    const response = await fetch("http://127.0.0.1:5000/auth/user/forget-password", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-              },
-        credentials: "include",
-        body: JSON.stringify(data),
+    const response = await fetch("http://127.0.0.1:5000/auth/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      // allow HTTP Only Set-Cookie to be forwarded from API Server to client and from client to API Server
+      body: JSON.stringify(data),
     });
 
+    if (response.ok) {
+      return {
+        error: null,
+        data: null,
+      };
+    } else {
+      const error = await response.json();
+      return {
+        error: error.error,
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: error.message,
+      data: null,
+    };
+  }
+}
+
+export async function register(data) {
+  try {
+    const response = await fetch("http://127.0.0.1:5000/auth/user/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(data),
+    });
+
+    console.log(response.status);
+    console.log(response.headers.entries());
+
+    if (response.ok) {
+      return {
+        error: null,
+        data: null,
+      };
+    } else {
+      const error = await response.json();
+      return {
+        error: error.error,
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: error.message,
+      data: null,
+    };
+  }
+}
+
+export async function logout() {
+  try {
+    const response = await fetch("http://127.0.0.1:5000/auth/user/logout", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    if (response.ok) {
+      return {
+        error: null,
+        data: null,
+      };
+    } else {
+      const error = await response.json();
+      return {
+        error: error.error,
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: error.message,
+      data: null,
+    };
+  }
+}
+
+export async function forgetPassword(data) {
+  try {
+    const response = await fetch(
+      "http://127.0.0.1:5000/auth/user/forget-password",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(data),
+      }
+    );
+
     const result = await response.json();
+    if (response.ok) {
+      return {
+        error: null,
+        data: null,
+      };
+    } else {
+      return {
+        error: result.error,
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: error.message,
+      data: null,
+    };
+  }
+}
+
+export async function resetPassword(data) {
+  try {
+    const response = await fetch(
+      "http://127.0.0.1:5000/auth/user/reset-password",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify(data),
+      }
+    );
+
+    const result = await response.json();
+    // response.ok false only when the status is 400.
     if (response.ok) {
       return {
         error: null,
