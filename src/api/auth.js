206e23e77a09cffd9a16431396b5859bd2690b65
sdrawkcabemaN <kinan.sutattyo@gmail.com>
2025-06-11 12:17:38 +0700

    accoutn-verification

--- src/api/auth.js
+++ src/api/auth.js
@@ -186,3 +186,36 @@ export async function resetPassword(data) {
     };
   }
 }
+
+export async function resendVerificationLink(data) {
+  try {
+    const response = await fetch("http://127.0.0.1:5000/auth/user/resent-verification-link", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      return {
+        error: null,
+        data: null,
+      };
+    } else {
+      return {
+        error: result.error,
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: error.message,
+      data: null,
+    };
+  }
+}
+
--- src/check-auth.js
+++ src/check-auth.js
@@ -5,7 +5,6 @@ export async function checkAuth(status = ["verified"]) {
   const { data: user } = await getAuthenticatedUser();
 
   if (!user || !status.includes(user.status)) {
-    window.location.href = "./login.html";
     return null;
   }
 
