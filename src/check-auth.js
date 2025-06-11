6e9f37614e512c9252cb168d417ac49e4b86c4e6
Jayaku Briliantio <jayakubriliantio@gmail.com>
2025-06-11 13:26:04 +0700

    feat: update check auth mechanism

--- src/check-auth.js
+++ src/check-auth.js
@@ -1,10 +1,10 @@
-
 import { getAuthenticatedUser } from "./api/auth.js";
 
 export async function checkAuth(status = ["verified"]) {
   const { data: user } = await getAuthenticatedUser();
 
   if (!user || !status.includes(user.status)) {
+    window.location.href = "/login.html";
     return null;
   }
 
