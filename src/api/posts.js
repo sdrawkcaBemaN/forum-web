740b643f3d181bfdca0ba5937d1aad78a49f1ab4
sdrawkcabemaN <kinan.sutattyo@gmail.com>
2025-07-02 14:32:59 +0700

    2/7/25

--- src/api/posts.js
+++ src/api/posts.js
@@ -25,6 +25,33 @@ export async function getAll() {
   }
 }
 
+export async function getPost(id) {
+  try {
+    const response = await fetch(`http://127.0.0.1:5000/post/${id}`, {
+      method: "GET",
+      credentials: "include",
+    });
+
+    const result = await response.json();
+    if (response.ok) {
+      return {
+        error: null,
+        data: result.data,
+      };
+    } else {
+      return {
+        error: result.error,
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: error.message,
+      data: null,
+    };
+  }
+}
+
 export async function createPost(data) {
   try {
     const response = await fetch("http://127.0.0.1:5000/post/", {
@@ -57,3 +84,68 @@ export async function createPost(data) {
     };
   }
 }
+
+export async function removePost(id) {
+  try {
+    const response = await fetch(`http://127.0.0.1:5000/post/${id}`, {
+      method: "DELETE",
+      credentials: "include",
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      return {
+        error: null,
+        data: result.data,
+      };
+    } else {
+      return {
+        error: result.error,
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: {
+        message: error.message,
+      },
+      data: null,
+    };
+  }
+}
+
+export async function updatePost(id, dto) {
+  try {
+    const response = await fetch(`http://127.0.0.1:5000/post/${id}`, {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dto),
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+      return {
+        error: null,
+        data: result.data,
+      };
+    } else {
+      return {
+        error: result.error,
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: {
+        message: error.message,
+      },
+      data: null,
+    };
+  }
+}
+
