ff6376d3a318d457ef1e0394e56cb42d95346e9b
sdrawkcabemaN <kinan.sutattyo@gmail.com>
2025-07-02 15:23:45 +0700

    get all

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,6 +1,6 @@
 export async function getAll() {
   try {
-    const response = await fetch("http://127.0.0.1:5000/post/user", {
+    const response = await fetch("http://127.0.0.1:5000/post", {
       method: "GET",
       credentials: "include",
     });
