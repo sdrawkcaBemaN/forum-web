16e0e22946bc8586d1ef97a5e63f0b3b4d4d1e55
sdrawkcabemaN <kinan.sutattyo@gmail.com>
2025-07-11 14:06:49 +0700

    likes, unlikes, details 11/7/25

--- src/api/posts.js
+++ src/api/posts.js
@@ -238,4 +238,63 @@ export async function getReplies(id) {
   }
 }
 
+export async function likePost(id) {
+  try {
+    const response = await fetch(`http://127.0.0.1:5000/post/${id}/like`, {
+      method: "POST",
+      credentials: "include",
+    });
+
+    const result = await response.json();
+    if (response.ok) {
+      return {
+        error: null,
+        data: result.data,
+      };
+    } else {
+      return {
+        error: result.error,
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: {
+        message: error.message,
+      },
+      data: null,
+    };
+  }
+}
+
+export async function unlikePost(id) {
+  try {
+    const response = await fetch(`http://127.0.0.1:5000/post/${id}/unlike`, {
+      method: "POST",
+      credentials: "include",
+    });
+
+    const result = await response.json();
+    if (response.ok) {
+      return {
+        error: null,
+        data: result.data,
+      };
+    } else {
+      return {
+        error: result.error,
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: {
+        message: error.message,
+      },
+      data: null,
+    };
+  }
+}
+
+
 
