ccdef817da6c4ed32bb9a6162a79f73b3c436832
sdrawkcabemaN <kinan.sutattyo@gmail.com>
2025-07-23 13:37:10 +0700

    follow/following, 23/7/25

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,6 @@
 export async function followUser(id) {
   try {
-    const response = await fetch(`http://127.0.0.1:5000/post/${id}/follow`, {
+    const response = await fetch(`http://127.0.0.1:5000/user/${id}/follow`, {
       method: "POST",
       credentials: "include",
     });
@@ -29,7 +29,7 @@ export async function followUser(id) {
 
 export async function unfollowUser(id) {
   try {
-    const response = await fetch(`http://127.0.0.1:5000/post/${id}/unfollow`, {
+    const response = await fetch(`http://127.0.0.1:5000/user/${id}/unfollow`, {
       method: "POST",
       credentials: "include",
     });
@@ -56,5 +56,30 @@ export async function unfollowUser(id) {
   }
 }
 
+export async function followStatus(id) {
+  try {
+    const response = await fetch(`http://127.0.0.1:5000/post/${id}/followstat`, {
+      method: "GET",
+      credentials: "include",
+    });
 
+    const result = await response.json();
+    if (response.ok) {
+      return {
+        error: null,
+        data: result.data,
+      };
+    } else {
+      return {
+        error: result.error,
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      error: error.message,
+      data: null,
+    };
+  }
+}
 
